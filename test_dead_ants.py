0a4d4d13d6c6bf7e65722df9f43dd0b669a8846f
Azriel72 <124940320+Azriel72@users.noreply.github.com>
2024-05-28 09:27:48 -0400

    Add files via upload

---
+++ test_dead_ants.py
@@ -0,0 +1,11 @@
+import pytest
+from hormigas_muertas import dead_ant_count
+
+def test_dead_ant_count():
+    assert dead_ant_count("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t") == 3
+
+def test_dead_ant_count2():
+    assert dead_ant_count(" ") == 0
+
+def test_dead_ant_count3():
+    assert dead_ant_count("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant") == 2
