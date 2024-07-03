a6670a44b73834e7aba578685fac3939c295f548
Azriel72 <124940320+Azriel72@users.noreply.github.com>
2024-07-03 18:34:13 -0400

    Update *_test.py

--- *_test.py
+++ *_test.py
@@ -5,7 +5,7 @@ def test_dead_ant_count():
     assert dead_ant_count("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t") == 3
 
 def test_dead_ant_count2():
-    assert dead_ant_count(" ") == 0
+    assert 1 == 0
 
 def test_dead_ant_count3():
     assert dead_ant_count("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant") == 2
